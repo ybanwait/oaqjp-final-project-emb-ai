53f28157c35a8f512c63b7d3001fcb2305f62ea7
yadwinder <yadwinder.banwait@gmail.com>
2025-01-08 01:00:39 -0500

    Static code analysis completed

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+"""
+This module contains function to analyze the text and detect emotion from text. 
+"""
 #import the required packages, methods
 from flask import Flask, render_template,request
 
@@ -5,26 +8,32 @@ from EmotionDetection.emotion_detection import emotion_detector
 
 app=Flask("Emotion Detector")
 
-# Set up the default page.
 @app.route("/")
 def index():
+    """
+    Set up the default page.
+    """
     return render_template('index.html')
 
 @app.route("/emotionDetector", methods=['GET'])
-def emotionDetector():
-    #function to analyze text entered by user
-    textToAnalyze = request.args.get('textToAnalyze')
-    result = emotion_detector(textToAnalyze)
+def analyze_emotions():
+    """
+    function to analyze text entered by user
+    """
+    text_to_analyze = request.args.get('textToAnalyze')
+    result = emotion_detector(text_to_analyze)
     #If dominant emotion is None then we send error message
-    if result['dominant_emotion'] == None:
+    if result['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
 
     # send back formatted output
-    formattedText = f"For the given statement, the system response is 'anger': {result['anger']}, 'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']}, 'sadness': {result['sadness']}."
-    formattedText = formattedText + f"The dominant emotion is {result['dominant_emotion']}."
-    
-    return formattedText
+    formatted_text = "For the given statement, the system response "
+    formatted_text += f"is 'anger': {result['anger']}, 'disgust': {result['disgust']}"
+    formatted_text += f", 'fear': {result['fear']}, 'joy': {result['joy']}"
+    formatted_text += f", 'sadness': {result['sadness']}."
+    formatted_text += f" The dominant emotion is {result['dominant_emotion']}."
+    return formatted_text
 
 # Finally run the app at port 5000.
 if __name__ == "__main__":
-    app.run(host='localhost', port=5000) 
+    app.run(host='localhost', port=5000)
