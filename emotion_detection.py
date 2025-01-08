39f7ea30664297dbabe1fb337fb926fdcb59ed90
yadwinder <yadwinder.banwait@gmail.com>
2025-01-07 20:20:45 -0500

    Added output formatting in emotion detector function

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,20 @@
-import requests
+import requests, json
 
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    jsonData={ "raw_document": { "text": text_to_analyse } }
-    result=requests.post(url, json=jsonData, headers=headers)
-    return result.text
+    jsonData = { "raw_document": { "text": text_to_analyse } }
+    resRaw = requests.post(url, json=jsonData, headers=headers)
+    resjson = json.loads(resRaw.text)  
+    emotions = {}
+    for key,value in resjson.items():
+        if key == 'emotionPredictions':
+            for j in resjson[key][0]:
+                if j == 'emotion':
+                    for emo, rating in resjson[key][0][j].items():
+                        emotions[emo] = rating            
+                    
+    emotionsList = sorted(emotions.items(), key=lambda x:x[1], reverse=True)
+    emotions = dict(emotionsList)
+    emotions['dominant_emotion'] = list(emotions.keys())[0] 
+    return emotions
